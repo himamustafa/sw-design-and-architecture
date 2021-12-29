2a33f47bc392465b0206dbf983b78bf45b2e1405
himamustafa <64609016+himamustafa@users.noreply.github.com>
2021-12-29 14:05:19 +0200

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -11,15 +11,53 @@ public class Admin extends Person1 {
 	}
 	String username;
 	String password;
-	//ArrayList <Admin> admins = new ArrayList <Admin>();
-	/*@Override
-	public void Register(Person a) {
-		// TODO Auto-generated method stub
-		admins.add((Admin) a);
-	}*/
-	/*Admin(String u,String p)
+
+
+public void verify(String driver_username,String driver_email) 
+{
+	for(int i =0;i<all_drivers.size();i++) {
+		if(all_drivers.get(i).username.equals(driver_username) && all_drivers.get(i).email.equals(driver_email) ) {
+			all_drivers.get(i).pending=false;
+		}
+	}
+}
+public ArrayList <Driver> list_allpending()
+{
+	ArrayList<Driver> pendingdrivers = new ArrayList <Driver>();
+	for(int i =0;i<all_drivers.size();i++) 
 	{
-		username = u;
-		password = p;
-	}*/
+		if(all_drivers.get(i).pending) 
+		{
+			pendingdrivers.add(all_drivers.get(i));
+		}
+	}
+	return pendingdrivers;
+}
+
+public Boolean login(Admin admin) 
+{
+for(int i=0;i<all_admins.size();i++) 
+{
+	if(all_admins.get(i).username.equals(admin.username)) 
+	{
+		if(all_admins.get(i).password.equals(admin.password)) 
+		{
+			return true;
+		}
+		else 
+			return false;
+	}
+}
+return false;
+
+}
+
+public void register(Admin admin) 
+{
+	all_admins.add(admin);
+}
+
+
+
+
 }
