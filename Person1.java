8ec194f2a7eb2a9aa7d7563699832db543f5c71d
Hania Abdalla <64498492+haniaabdalla@users.noreply.github.com>
2021-12-29 23:07:35 +0200

    Update Person1.java

--- Person1.java
+++ Person1.java
@@ -1,4 +1,4 @@
-package app;
+package com.example.uber;
 
 import java.util.ArrayList;
 
@@ -10,89 +10,28 @@ public class Person1
 	public static ArrayList<Admin> all_admins = new ArrayList  <Admin>();
 	public static ArrayList<Driver> all_drivers = new ArrayList  <Driver>();
 	public Subject s = new MessagePublisher();
-	 Person1(String x, String y){
+	
+	
+	Person1(){
+		
+	 }
+	
+	
+	Person1(String x, String y){
 		 username=x;
 		 password=y;
 	 }
+	public Boolean login(Person1  person) 
+	{
+		return true;
+	}
 	
-	public Boolean Login(Person1  f) 
-	{
-		if(f instanceof Driver) 
-		{
-			for(int i=0;i<all_drivers.size();i++) 
-			{
-				if(all_drivers.get(i).username.equals(f.username)) 
-				{
-					if(all_drivers.get(i).password.equals(f.password) && !(all_drivers.get(i).pending)) 
-					{
-						return true;
-					}
-					else 
-						return false;
-				}
-			}
-			return false;
-		}
-		else if(f instanceof User) 
-		{
 	
-			for(int i=0;i<all_users.size();i++) 
-			{
-				if(all_users.get(i).username.equals(f.username)) 
-				{
-					
-					if(all_users.get(i).password.equals(f.password)) 
-					{
-						return true;
-					}
-					else 
-						return false;
-				}
-			}
-			return false;
-		}
-		else if(f instanceof Admin) 
-		{
-			for(int i=0;i<all_admins.size();i++) 
-			{
-				if(all_admins.get(i).username.equals(f.username)) 
-				{
-					if(all_admins.get(i).password.equals(f.password)) 
-					{
-						return true;
-					}
-					else 
-						return false;
-				}
-			}
-			return false;
-		}
-		return null;
-	}
-	public ArrayList <Driver> list_allpending()
+	public void register(Person1 person) 
 	{
-		ArrayList<Driver> pendingdrivers = new ArrayList <Driver>();
-		for(int i =0;i<all_drivers.size();i++) 
-		{
-			if(all_drivers.get(i).pending) 
-			{
-				pendingdrivers.add(all_drivers.get(i));
-			}
-		}
-		return pendingdrivers;
+		
 	}
-	public void verify(String driver_username,String driver_email) 
-	{
-		for(int i =0;i<all_drivers.size();i++) {
-			if(all_drivers.get(i).username.equals(driver_username) && all_drivers.get(i).email.equals(driver_email) ) {
-				all_drivers.get(i).pending=false;
-			}
-		}
-	}
-	public void add_to_favourites(Driver r,String area) 
-	{
-		r.favourite_areas.add(area);
-	}
+	
 	public void adding_Observers(ArrayList <User>u,ArrayList<Driver> d) 
 	{
 		for(int i=0;i<u.size();i++) 
@@ -105,3 +44,4 @@ public class Person1
 		}
 	}
 }
+	
