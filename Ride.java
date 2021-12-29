5ac00e0d39f82180b24258228002a9c14eba04c2
Hania Abdalla <64498492+haniaabdalla@users.noreply.github.com>
2021-12-29 23:08:00 +0200

    Update Ride.java

--- Ride.java
+++ Ride.java
@@ -1,28 +1,55 @@
-package app;
+package com.example.uber;
 
 public class Ride 
 {
-	String source;
-	String Destination;
-	User user;
+	private DistanceTime distance;
+	static int rideId;
+	int Id=0;
+	static String source;
+	static String Destination;
+	static User user;
 	Driver driver;
-	int price;
+	static double price;
+	boolean Ride_Completed= false;
+	double RideRate=0;
+	Ride()
+	{
+		
+	}
 	Ride(String s,String d, User u)
 	{
 		source=s;
 		Destination=d;
 		user =u;
 		price=-1;
+		Id++;
+		rideId = Id;
 	}
-	Ride(String s,String d, Driver u)
-	{
-		source=s;
-		Destination=d;
-		driver =u;
-		price=-1;
-	}
-	public void Set_price(int p) 
+static public void set_ride(String s,String d, User u)
+{
+	source=s;
+	Destination=d;
+	user =u;
+	price=-1;
+}
+	public void Set_price(double p) 
 	{
 		price =p;
 	};
+	public Ride getbyusername(String username) 
+	{
+		Ride r =null;
+		for(int i =0;i<user.rides.size();i++) 
+		{
+			if(user.rides.get(i).user.username.equals(username)) 
+			{
+				r=user.rides.get(i);
+			}
+		}
+		return r;
+	}
+	public void setDistance(DistanceTime distance) {
+		   this.distance = distance;
+		}
+	
 }
