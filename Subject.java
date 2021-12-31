ea8c9377b1ac04b0bae35e7cbcdc435ef755ac15
hebashahib <64308264+hebashahib@users.noreply.github.com>
2021-12-31 21:21:17 +0200

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -1,10 +1,11 @@
-package app;
+package com.example.uber;
 
 import java.util.ArrayList;
-
+import java.util.List;
 public interface Subject 
 {
+	List<Observer> obs = new ArrayList<>();
     public void attach(Observer o);
     public void detach(Observer o);
-    public void notifyUpdate(Ride r);
+    public void notifyUpdate();
 }
